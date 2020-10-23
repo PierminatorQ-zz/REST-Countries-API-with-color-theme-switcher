ba32ccd8e90f65b832ca5ba2fddee5a13f29bd15
PierminatorQ <pierrequintero@gmail.com>
2020-10-23 11:52:25 -0300

    borders added

--- js/printModal.js
+++ js/printModal.js
@@ -1,26 +1,37 @@
 function printModal(country){
   const imagen = '<img src="'+country.flag+'" alt="" class="img-fluid">'
+  
   let currencies =[]
   let languages = []
+  let borders=[]
+
+
+  
   country.currencies.forEach(function(currency){
     currencies.push(currency.name) 
   })
 
+
+
   country.languages.forEach(function(language){
     languages.push(language.name)
   })
+      
+  
+  country.borders.forEach(function(border){
+    countries.filter(element =>{
+      if (element.alpha3Code == border){
+      borders.push(element.name)
+      }
+    })
 
-  console.log(countries)
-  //country.borders.forEach(function(border){
+  })
 
-
-    //$('#borderModal').append('<span class="border-class">'+border+', </span>')
- // })
-
+  
 
 
   $('#nombreModal').html(country.name)
-  //$('#countryModal').html(''+country.name+'')
+  $('#borderModal').append(borders.join(", "))
   $('#modalTitle').html(country.name)
   $('#imageModal').append(imagen)
   $('#nativeModal').html(country.nativeName)
